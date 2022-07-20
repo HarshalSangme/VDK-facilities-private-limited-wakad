d32132a741f3381289b1f876dc7cf9ac61314484
HarshalSangme <harshalsangme10@gmail.com>
2022-07-20 18:36:07 +0530

    Client section Contact us created -- Responsivness

---
+++ pages/contactUsPages/contactUsForm.php
@@ -0,0 +1,296 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+                integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+                crossorigin="anonymous">
+
+        
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css'>
+
+        <!-- Manual css -->
+        <link rel="stylesheet" href="../../assets/css/general/homepage.css">
+        <link rel="stylesheet" href="../../assets/css/general/footer.css">
+        <link rel="stylesheet" href="../../assets/css/general/contactUs.css">
+ 
+        <!-- Font awesome -->
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+        <title>Hello, world!</title>
+</head>
+
+<body>
+
+
+<nav class="navbar fixed-top navbar-expand-lg">
+                <div class="container">
+                        <a class="navbar-brand" href="/">
+                                <img src="../../assets/images/logo/logo.png" alt="" width="120" height="50"
+                                        class="d-inline-block align-text-top">
+                        </a>
+
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                                aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="navbar-toggler-icon"></span>
+                        </button>
+
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+                                <ul class="navbar-nav ms-auto ml-auto ">
+
+                                        <li class="nav-item">
+                                                <a class=" nav-list-color nav-link" aria-current="page"
+                                                        href="/">HOME</a>
+                                        </li>
+
+                                        <li class="nav-item dropdown">
+                                                <a class=" nav-list-color nav-link dropdown-toggle" href="#"
+                                                        id="navbarDropdown" role="button" data-bs-toggle="dropdown"
+                                                        aria-expanded="false">
+                                                        SERVICES
+                                                </a>
+                                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                                        <li><a class="dropdown-item"
+                                                                        href="pages/servicesPages/facilitiy-management-services.php">Facilitiy management services</a></li>
+                                                        <li><a class="dropdown-item"
+                                                                        href="pages/servicesPages/machanized-housekeeping.php">
+                                                                        Machanized housekeeping </a></li>
+                                                        <li><a class="dropdown-item"
+                                                                        href="pages/servicesPages/solid-waste-management.php">
+                                                                        Solid waste management       
+                                                                </a></li>
+                                                        <li><a class="dropdown-item"
+                                                                        href="pages/servicesPages/landspace-gardening.php">
+                                                                        Landspace Gardening
+                                                                        </a></li>
+                                                        <li><a class="dropdown-item"
+                                                                        href="pages/servicesPages/earth-works.php">Earth Works</a></li>
+                                                        <li><a class="dropdown-item"
+                                                                        href="pages/servicesPages/serviceSix.html">SERVICE SIX</a></li>
+                                                        <!-- <li><a class="dropdown-item"
+                                                                        href="pages/whatWeOfferPages/technical-services.html">TECHNICAL
+                                                                        SERVICES</a></li>
+                                                        <li><a class="dropdown-item"
+                                                                        href="pages/whatWeOfferPages/finance-and-accounting.html">FINANCE
+                                                                        AND ACCOUNTING</a>
+                                                        </li>
+                                                        <li><a class="dropdown-item"
+                                                                        href="pages/whatWeOfferPages/sales.html">SALES</a>
+                                                        </li> -->
+                                                </ul>
+                                        </li>
+
+                                        <li class="nav-item dropdown">
+                                                <a class=" nav-list-color nav-link" href="pages/navbarPages/aboutUs.html">ABOUT US</a>
+                                        </li>
+
+                                        
+             
+
+                                        <!-- <li class="nav-item">
+                                                <a class="nav-list-color nav-link" href="portfolio.html">NEWSROOM</a>
+                                        </li>
+
+                                        <li class="nav-item">
+                                                <a class=" nav-list-color nav-link" href="team-members.html">CAREERS</a>
+                                        </li> -->
+
+                                        <li class="nav-item">
+                                                <a class="nav-list-color nav-link"
+                                                        href="pages/contactUsPages/contactUsForm.php">CONTACT US</a>
+                                        </li>
+
+
+                                </ul>
+                                <!-- <form class="d-flex">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success" type="submit">Search</button>
+            </form> -->
+                        </div>
+                </div>
+</nav>
+
+<section id="contact" class="contact">
+    <div id="carouselExampleSlidesOnly" class="carousel slide carousel-fade" data-ride="carousel">
+      <div class="carousel-inner" style="max-height:400px !important;">
+        <div class="carousel-item active">
+          <img class="d-block w-100" src="https://www.tajhotels.com/content/dam/luxury/tle/TLE_Images_Folderised/2_Rooms_Suites/Luxury_Room/3X2/WOGLI/Luxury%20Room%20Sea%20View.jpg/jcr:content/renditions/cq5dam.web.756.756.jpeg" alt="First slide">
+        </div>
+      </div>
+    </div>
+    <div class="container pt-5">
+      <div class="section-title-contact">
+        <h2>Contact us</h2>
+      </div>
+      <div class="row mt-1">
+        <div class="col-lg-4">
+          <div class="info">
+            <div class="address">
+            <i class="fa fa-map-marker"></i>
+              <h4>Location:</h4>
+              <p>
+                <b>Corporate office :-</b>
+                <br>
+                Office No 408 The Address Commercia Mall
+                <br>
+                Shanker Kalate Nagar 
+                <br>
+                Wakad - 411057
+        
+              </p>
+
+            </div>
+            <div class="email">
+            <i class="fa fa-envelope"></i>
+              <h4>Email:</h4>
+              <p>
+                vdkfacilites@gmail.com
+              </p>
+            </div>
+            <div class="phone">
+            <i class="fa fa-phone" ></i>  
+              <h4>Call:</h4>
+              <p>
+                +91 9922-972-137
+              </p>
+            </div>
+          </div>
+        </div>
+        <!-- -------------------------Contact Us form -->
+        <div class="col-lg-8 mt-5 mt-lg-0">
+          <div class="info">Contact us now !</div>
+          <form action="sendEmail.php" method="post" name="form" class="form-box php-email-form">
+            <div class="row">
+              <div class="col-md-6 form-group">
+                <label for="name">Name</label><br>
+                <input type="text" name="name" class="inp form-control" placeholder="Enter Your Name" required>
+              </div>
+              <div class="col-md-6 form-group mt-3 mt-md-0">
+                <label for="email">Email ID</label><br>
+                <input type="email" name="email" class="inp form-control" placeholder="Enter Your Email" required>
+              </div>
+            </div>
+            <div class="form-group mt-3">
+              <label for="phone">Phone</label><br>
+              <input type="tel" name="phone" class="inp form-control" placeholder="Enter Your Phone" required>
+            </div>
+            <div class="form-group mt-3">
+              <label for="message">Message</label><br>
+              <textarea name="msg" class="msg-box form-control" placeholder="Enter Your Message Here..." required></textarea>
+            </div>
+            <div class="my-3 text-center">
+              <input type="submit" name="submit" value="Send" class="sub-btn btn send-btn btn-dark">
+            </div>
+          </form>
+        </div>
+</section>
+
+
+<footer class="footer-container">
+                <div class="footer-top footer">
+                    <div class="container ft-container-main">
+                        
+                        <div class="row">
+
+                            <div class="col-md-4 col-lg-4 footer-about wow fadeInUp">
+
+                                <img src="../../assets/images/logo/logo.png" alt="" width="120" height="50" style="margin-bottom: 20px;">
+
+                                <p class="ft-row-1-p">
+                                        Vdk Group Today Is Amongst The Top Facility Management Companies In Pune And A
+                                        One-stop-shop For All Your Facility Management Requirements Related To Business.
+                                </p>
+                                
+                                <p><a href="#">Services</a></p>
+                                <p><a href="#">Our Team</a></p>
+                                <p><a href="#">About us</a></p>
+                                <p><a href="#">Contact us</a></p>
+                                <p><a href="#">Polices</a></p>
+
+                            </div>
+
+                            <div class="col-md-4 col-lg-4 offset-lg-1 footer-contact wow fadeInDown" style="color: white;">
+                                
+                                <h3 style="color: white; margin-bottom: 20px;">Contact</h3>
+
+                                <p><i class="fa fa-map-marker" aria-hidden="true"></i> Corporate Office :
+                                     <!-- <br> -->
+                                     Office No 408 
+                                    <br> 
+                                    The Address Commercia Mall
+                                    <br>
+                                    Shanker Kalate Nagar 
+                                    <br>
+                                    Wakad - 411057
+                                    </p>
+                                
+                                <p><i class="fa fa-whatsapp"></i> Whatsapp: +91-9657550522</p>
+                                <p><i class="fa fa-phone"></i> Phone: +91-9922733222</p>
+                                <p><i class="fa fa-envelope"></i> Email: <a href="mailto:hello@domain.com">info@nexottel.com</a>
+                                </p>
+
+                            </div>
+
+                            <div class="col-md-4 col-lg-3 footer-social wow fadeInUp " style="color: white;" >
+                                <h3 style="margin-bottom: 20px;">Follow us</h3>
+                                <p style="margin-bottom: 40px;">
+                                    <a href="#"><i class="fa fa-facebook"></i></a>
+                                    <a href="#"><i class="fa fa-twitter"></i></a>
+                                    <a href="#"><i class="fa fa-google-plus"></i></a>
+                                    <a href="#"><i class="fa fa-instagram"></i></a>
+                                </p>
+
+                                <button id="btn-enquiry"> ENQUIRY </button>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+                </div>
+        
+            </footer>
+
+
+
+
+        <!-- Optional JavaScript; choose one of the two! -->
+
+        <!-- Option 1: Bootstrap Bundle with Popper -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+                integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+                crossorigin="anonymous"></script>
+
+        <!-- End Contact Section -->
+  <script src="../../assets/js/main.js"></script>
+  <!-- Optional JavaScript; choose one of the two! -->
+  <!-- Option 1: Bootstrap Bundle with Popper -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+        <!-- Option 2: Separate Popper and Bootstrap JS -->
+        <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+
+    <!-- AOS LIBRARY SCRIPT -->
+
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script>
+      AOS.init();
+    </script>
+
+</body>
+
+</html>
+
